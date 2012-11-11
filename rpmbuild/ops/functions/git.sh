f1605f3bcb06cab6772f83531ea532d453793ee0
Masahito Yoshida <masahito@axsh.net>
2012-11-11 23:29:38 +0900

    rpmbuild: add commit_cd to functions/git.

--- rpmbuild/ops/functions/git.sh
+++ rpmbuild/ops/functions/git.sh
@@ -3,6 +3,7 @@
 # requires:
 #  bash
 #  git, sed
+#  date
 #
 # imports:
 #
@@ -14,4 +15,9 @@ function current_branch_name() {
 function short_hash() {
   local build_id=${1:-HEAD}
   git log ${build_id} -n 1 --pretty=format:"%h"
+}
+
+function commit_cd() {
+  local short_hash=${1:-HEAD}
+  date --date="$(git log ${git_version} -n 1 --pretty=format:"%cd" --date=iso)" +%Y%m%d%H%M%S
 }
