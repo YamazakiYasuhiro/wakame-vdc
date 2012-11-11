d53cd65df19c1b071e43e56a00617a49c52c4399
Masahito Yoshida <masahito@axsh.net>
2012-11-11 23:25:43 +0900

    rpmbuild: add short_hash to functions/git.

--- rpmbuild/ops/functions/git.sh
+++ rpmbuild/ops/functions/git.sh
@@ -10,3 +10,8 @@
 function current_branch_name() {
   git branch --no-color | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 }
+
+function short_hash() {
+  local build_id=${1:-HEAD}
+  git log ${build_id} -n 1 --pretty=format:"%h"
+}
